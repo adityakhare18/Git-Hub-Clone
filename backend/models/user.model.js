5f17a1e8db01814fe5d156c4dc06ecf53ee26241
sarthakshishodia20 <sarthakshishodia2004@gmail.com>
2025-01-16 20:23:02 +0530

    AuthenticationInitialisation

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -9,12 +9,6 @@ const userSchema = new mongoose.Schema(
       trim: true,
       minlength: [3, "Username must be at least 3 characters long"],
     },
-    fullname: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: [3, "Fullname must be at least 3 characters long"],
-    },
     email: {
       type: String,
       required: true,
@@ -28,24 +22,6 @@ const userSchema = new mongoose.Schema(
       trim: true,
       minlength: [6, "Password must be at least 6 characters long"],
     },
-    bio: {
-      type: String,
-    },
-    profilePhoto: {
-      type: String,
-    },
-    followers: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    following: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
   },
   { timestamps: true }
 );
