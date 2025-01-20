22a047869fae57238eb011bc0fcb4a9547c211e9
kushalyadav2 <kushalyadav589@gmail.com>
2025-01-20 17:41:42 +0530

    login and signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
-    <div>
-      App component
+    <div className="flex">
+      <Sidebar />
+      <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+        <Routes>
+          <Route path="/" element={<Homepage />} /> {/* Corrected from <route> to <Route> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
     </div>
   )
 }
