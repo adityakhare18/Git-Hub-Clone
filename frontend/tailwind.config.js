91aee53edf50f0d0e8b0b91b198a8d3a460b39a1
kushalyadav2 <kushalyadav589@gmail.com>
2025-01-20 17:48:57 +0530

    image logo and tailwing css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-green': '#64D3BA',
+        'custom-white': '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 }
