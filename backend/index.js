42253e9f8b976c16ce7d57afdcc1dbf4f255def7
adityakhare18 <khareaditya0105@gmail.com>
2025-02-12 11:13:18 +0530

    Integrate AWS S3 for commit storage, add server start command, and implement error handling in pushRepo function

--- backend/index.js
+++ backend/index.js
@@ -45,6 +45,7 @@ const { pullRepo } = require('./controllers/pull.js')
 const { revertRepo } = require('./controllers/revert.js')
 
 yargs(hideBin(process.argv))
+  .command("start", "Start a new server", {}, startServer)
   .command("init", "Initialise a new repository", {}, initRepo)
   .command(
     "add <file>",
@@ -87,3 +88,8 @@ yargs(hideBin(process.argv))
   )
   .demandCommand(1, "You need at least one command")
   .help().argv;
+
+
+function startServer(){
+  console.log("Server logic start");
+}
