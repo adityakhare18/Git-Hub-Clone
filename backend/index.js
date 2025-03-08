53db65aa06104664123667f505e1a60336b859af
adityakhare18 <khareaditya0105@gmail.com>
2025-03-08 13:30:02 +0530

    Add demo files and update package dependencies; refactor server initialization with Socket.IO and MongoDB integration

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,21 @@
-// import express from 'express';
-// import session from 'express-session';
-// import passport from 'passport';
-// import dotenv from 'dotenv'; // Correct import statement
-// dotenv.config();
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const http = require("http");
+const { Server } = require("socket.io");
 
-// import userRouter from './routes/user.route.js';
-// import { connectToDB } from './config/db.js';
-
-// // Connect to the database
-// connectToDB();
-
-// const app = express(); // Use `app` instead of `router` for the main application
-
-// // Middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET || 'secret',
-//     resave: false,
-//     saveUninitialized: false,
-//   })
-// );
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Route handling
-// app.use('/user', userRouter);
-
-// // Start server
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server is running on port ${process.env.PORT}`);
-// });
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 
 const { initRepo } = require("./controllers/init.js");
 const { addRepo } = require("./controllers/add.js");
-const { commitRepo } = require('./controllers/commit.js')
-const { pushRepo } = require('./controllers/push.js')
-const { pullRepo } = require('./controllers/pull.js')
-const { revertRepo } = require('./controllers/revert.js')
+const { commitRepo } = require("./controllers/commit.js");
+const { pushRepo } = require("./controllers/push.js");
+const { pullRepo } = require("./controllers/pull.js");
+const { revertRepo } = require("./controllers/revert.js");
 
 yargs(hideBin(process.argv))
   .command("start", "Start a new server", {}, startServer)
@@ -51,12 +24,12 @@ yargs(hideBin(process.argv))
     "add <file>",
     "Add a file to a repository",
     (yargs) => {
-      yargs.positional("file",{
-        describe:"File to add to a staging area",
-        type:"string",
+      yargs.positional("file", {
+        describe: "File to add to a staging area",
+        type: "string",
       });
     },
-    (argv)=>{
+    (argv) => {
       addRepo(argv.file);
     }
   )
@@ -64,34 +37,81 @@ yargs(hideBin(process.argv))
     "commit <message>",
     "Commit files in the staging area",
     (yargs) => {
-      yargs.positional("message",{
-        describe:"Commit message",
-        type:"string",
+      yargs.positional("message", {
+        describe: "Commit message",
+        type: "string",
       });
     },
-    (argv)=>{
+    (argv) => {
       commitRepo(argv.message);
     }
   )
-  .command("push","Push commits to S3", {}, pushRepo)
-  .command("pull","Pull commits from S3", {}, pullRepo)
+  .command("push", "Push commits to S3", {}, pushRepo)
+  .command("pull", "Pull commits from S3", {}, pullRepo)
   .command(
     "revert <commitID>",
     "Revert to a specific commit",
     (yargs) => {
-      yargs.positional("commitID",{
-        describe:"Commit ID to revert to",
-        type:"string",
+      yargs.positional("commitID", {
+        describe: "Commit ID to revert to",
+        type: "string",
       });
     },
-    (argv)=>{
+    (argv) => {
       revertRepo(argv.commitID);
     }
   )
   .demandCommand(1, "You need at least one command")
   .help().argv;
 
+function startServer() {
+  const app = express();
+  const port = process.env.PORT || 3000;
 
-function startServer(){
-  console.log("Server logic start");
+  app.use(bodyParser.json());
+  app.use(express.json());
+
+  const mongoURI = process.env.MONGODB_URL;
+  // app.listen(port, () => {
+  //   console.log(`Server is running on port ${port}`);
+  // });
+
+  mongoose.connect(mongoURI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Unable to connect to Database ",err));
+
+  app.use(cors({ origin : "*" })); // star means our req can come from anywhere
+  app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+
+
+  let user = "test";
+  const httpServer = http.createServer(app);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("Connection", (socket) => {
+    socket.on("joinRoom",(userId)=>{
+      user = userId;
+      console.log("====");
+      console.log(user);
+      console.log("====");  
+      socket.join(userId)
+    })
+  })
+
+  const db = mongoose.connection;
+
+  db.once("open", async () => {
+    console.log("CRUD operations called");
+  });
+
+  httpServer.listen(port, () => {
+    console.log(`Socket is running on port ${port}`);
+  });
 }
